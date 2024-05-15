7c07121aa8d5cb4a5d3ce19130ee639e171037bd
Neil Studd <neilstudd@users.noreply.github.com>
2024-05-15 08:20:03 +0100

    Make GH Actions happier with working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,9 @@ git_email="$4"
 mkdir -p "$(dirname "$file_in_repo")"
 wget "$url" -O "$file_in_repo"
 
-git config user.name "$git_name"
-git config user.email "$git_email"
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
+git config --global --add safe.directory /github/workspace
 
 git add "$file_in_repo"
 git commit -m "Update $(basename "$file_in_repo") from $url"
